13e9a815a30a36599dd48eb191acc48d73f37a48
toledobrendo <toledo.brendo@gmail.com>
2020-11-18 00:57:29 +0800

    added php sample

---
+++ php-db/phpmysql.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Sample PHP MySQL</title>
+    </head>
+    <body>
+        <h3>Values inside sample.sample_table</h3>
+        <?php
+            define('DB_HOST_IP', '127.0.0.1');
+            define('DB_PORT', '3306');
+            define('DB_USERNAME', 'btoledo');
+            define('DB_PASSWORD', '123qwe');
+            define('DB_NAME', 'sample');
+
+            @ $db = new mysqli(DB_HOST_IP.':'.DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+            $query = 'select * from sample_table;';
+            $stmt = $db->prepare($query);
+
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            while ($row = $result->fetch_assoc()) {
+                echo '<p>ID: '.$row['id'].' - Value: '.$row['value'];
+            }
+        ?>
+    </body>
+</html>
