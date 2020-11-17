6eb358f959b4cac05d5cc73f6121a39932d4f753
toledobrendo <toledo.brendo@gmail.com>
2020-11-18 01:17:46 +0800

    added php sample

--- php-db/phpmysql.php
+++ php-db/phpmysql.php
@@ -7,7 +7,7 @@
         <?php
             define('DB_HOST_IP', '127.0.0.1');
             define('DB_PORT', '3306');
-            define('DB_USERNAME', 'btoledo');
+            define('DB_USERNAME', 'bptoledo');
             define('DB_PASSWORD', '123qwe');
             define('DB_NAME', 'sample');
 
